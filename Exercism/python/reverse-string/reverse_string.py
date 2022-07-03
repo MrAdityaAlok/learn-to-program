c8421e3a54113151d3666daef29db1ed366cc6c3
Aditya Alok <dev.aditya.alok@gmail.com>
2022-07-03 10:25:53 +0530

    path(Exercism/python): add new exercise `reverse-string`
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

---
+++ Exercism/python/reverse-string/reverse_string.py
@@ -0,0 +1,13 @@
+"""Solution for `Reverse String` exercise."""
+
+
+def reverse(text: str) -> str:
+    """Reverses a string.
+
+    Args:
+        text: String to be reversed.
+
+    Returns:
+        Reversed string.
+    """
+    return text[::-1]
